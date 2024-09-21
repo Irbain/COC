cea068a7198c3261bc3e578a8e939953b7764973
Irbain <contact@irbaine.com>
2024-09-21 20:59:33 +0100

    Done

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export const metadata: Metadata = {
   // ROBOT
   robots: {
-    index: false,
+    index: true,
     nocache: true,
     // we can add other properties
   },
